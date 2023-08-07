03dcae073e0d0471d9c14ce9b91ad266745a357e
ryo-c-watanabe <ryo.c.watanabe@dena.com>
2023-08-07 11:14:16 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Ryo Watanabe on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
